349de3ba896cd7743618dd7e35512744594d1c97
toughzaa <35199865+toughzaa@users.noreply.github.com>
2018-01-07 20:05:12 +0100

    Create TreeHighestMapper.java

---
+++ 5.3/TreeHighestMapper.java
@@ -0,0 +1,37 @@
+package cs.bigdata.Tutorial2;
+
+import org.apache.hadoop.mapreduce.Job;  
+import org.apache.hadoop.io.*;        
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+
+public class TreeHighestMapper extends Mapper<IntWritable, Text, Text, IntWritable> {
+
+	//private Text word = new Text();
+
+	protected void map(Text keyE, IntWritable valE, Context context) throws IOException,InterruptedException
+    {
+		String content = keyE.toString();
+		String[] lines = content.split("\n");
+
+		for(String line: lines )
+		{
+			String[] splitted = line.split(";(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+			
+			// Extract the tree's type
+			String height = splitted[6];
+			
+			if(height == null | height.length() == 0) {
+				height = " NA ";
+			}
+			
+			Text keyS = new Text(height);
+			IntWritable valS = new IntWritable(1);
+			context.write(keyS, valS);
+		}
+    }
+}
