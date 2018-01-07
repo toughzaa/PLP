394b9b3ca5fe9a06e00037ac3be658a45ea3cd88
toughzaa <35199865+toughzaa@users.noreply.github.com>
2018-01-07 20:06:39 +0100

    Create TreesCountMapper.java

---
+++ 5.3/TreesCountMapper.java
@@ -0,0 +1,37 @@
+package cs.bigdata.Tutorial2;
+
+import org.apache.hadoop.mapreduce.Job;  
+import org.apache.hadoop.io.*;        
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+
+public class TreesCountMapper extends Mapper<IntWritable, Text, Text, IntWritable> {
+
+	//private Text word = new Text();
+
+	protected void map(Text keyE, IntWritable valE, Context context) throws IOException,InterruptedException
+    {
+		String content = keyE.toString();
+		String[] lines = content.split("\n");
+
+		for(String line: lines )
+		{
+			String[] splitted = line.split(";(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+			
+			// Extract the tree's type
+			String type = splitted[4];
+			
+			if(type == null | type.length() == 0) {
+				type = " NA ";
+			}
+			
+			Text keyS = new Text(type);
+			IntWritable valS = new IntWritable(1);
+			context.write(keyS, valS);
+		}
+    }
+}
